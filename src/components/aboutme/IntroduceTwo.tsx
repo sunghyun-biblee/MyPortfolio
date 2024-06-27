f87d9b4a2600bf78dacffe5d593c5d456e6734cb
sunghyun1014 <sunghyun1014@naver.com>
2024-06-28 02:04:46 +0900

    추가: instroduce 컴포넌트 디자인추가 및 애니메이션 추가

--- src/components/aboutme/IntroduceTwo.tsx
+++ src/components/aboutme/IntroduceTwo.tsx
@@ -2,24 +2,36 @@ import React, { useEffect, useState } from "react";
 import preview from "assets/img/profileImg.jpg";
 import styled from "styled-components";
 import { ScrollMoveType } from "components/projects/Projects";
-
+import MyQR from "assets/img/myQR.jpg";
+import Arrow from "assets/img/ClickArrow.svg";
 interface IIntroduce {
   value: ScrollMoveType;
 }
 
 export const IntroduceTwo = ({ value }: IIntroduce) => {
   const [isFlip, setIsFlip] = useState<boolean>(false);
-
+  const [isClick, setIsClick] = useState<boolean>(false);
   useEffect(() => {
-    const interval = setInterval(() => {
-      setIsFlip((prev) => !prev);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [isFlip]);
+    if (isClick && isFlip) {
+      return;
+    } else {
+      const interval = setInterval(() => {
+        setIsFlip((prev) => !prev);
+      }, 2500);
+      return () => clearInterval(interval);
+    }
+  }, [isFlip, isClick]);
 
   const handleImgClick = () => {
-    setIsFlip(!isFlip);
+    if (isFlip) {
+      setIsFlip(false);
+      // setIsClick((prev) => !prev);
+    } else {
+      setIsFlip(true);
+      // setIsClick((prev) => !prev);
+    }
   };
+  console.log(isClick);
   return (
     <div
       className="h-[100dvh] relative  w-[100%]  box-border overflow-hidden 
@@ -29,54 +41,93 @@ export const IntroduceTwo = ({ value }: IIntroduce) => {
       ref={value.element}
     >
       <div
-        className="flex h-[100vh] lg:max-w-[1200px] w-[100%] justify-around items-center text-white mx-auto my-0
+        className="flex h-[100vh] lg:max-w-[1200px] w-[100%] justify-around items-center text-white mx-auto lg:mt-[0px] mysm:mt-[100px] transition-all duration-700
       animate-introExpand-container   relative
       transform translate-x-[1400px]"
         id="container"
       >
         <div className="w-[100%] " id="container_content ">
           <div className="flex justify-center">
-            <div className="mx-[50px] w-[50%]  " id="container_outer_img">
-              <div
-                className=" animate-introPopUp-profileImg
-                opacity-0
+            <div
+              className=" animate-introPopUp-profileImg mysm:mt-[50px] lg:mt-[0px]
+                opacity-0 w-[330px] h-[530px] relative
                  flex justify-center items-center cursor-pointer"
-                id="img-inner"
-                onClick={handleImgClick}
-              >
-                <FlipBox value={isFlip}>
-                  <FlipCardFront className="rounded-lg shadow-2xl  overflow-hidden">
-                    <Img src={preview} alt="img" id="container_img" />
-                  </FlipCardFront>
-
-                  <FlipCardBack className="rounded-lg shadow-2xl overflow-hidden">
-                    <ul className="w-[100%] h-[100%] bg-[#E6F4F1] text-black">
-                      <li>
-                        <span>조성현</span>
-                      </li>
-                      <li>
-                        <span>1999.10.14</span>
-                      </li>
-                      <li>
-                        <span>대구광역시 남구</span>
-                      </li>
-                      <li>
-                        <span>계명문화대학교 졸업</span>
-                        <span>2018.03~2020.08</span>
-                      </li>
-                      <li>
-                        <span>멀티캠퍼스 웹 개발자 파트 수료</span>
-                        <span>2023.08~2024.02</span>
-                      </li>
-                    </ul>
-                  </FlipCardBack>
-                </FlipBox>
+              id="img-inner"
+              onClick={handleImgClick}
+              onMouseEnter={() => {
+                setIsClick(true);
+              }}
+              onMouseLeave={() => {
+                setIsClick(false);
+              }}
+            >
+              <div className="absolute -top-12 flex items-center">
+                <span className="text-xl font-semibold mr-3">Click Card !</span>
+                <img
+                  src={Arrow}
+                  alt="arrow"
+                  className="w-10 h-10 animate-bounce"
+                />
               </div>
+              <FlipBox value={isFlip} className=" relative  shadow-2xl ">
+                {/* <Edge className="w-[35px] h-[35px] absolute bg-[#158DC0] -bottom-5 right-12 z-[2] transform rotate-[45deg] "></Edge> */}
+                <FlipCardFront className="rounded-lg overflow-hidden relative">
+                  <Edge />
+                  <Img src={preview} alt="img" id="container_img" />
+                </FlipCardFront>
+
+                <FlipCardBack className="rounded-lg overflow-hidden  bg-blue-400 flex relative">
+                  <Edge />
+                  <div className=" ml-[8%] w-[17%] h-[100%] bg-blue-400/50 z-[3] brightness-[55%]"></div>
+                  <ul className="w-[75%] h-[100%] right-0  text-white z-[4] box-border p-3 text-left">
+                    <Li className="text-right font-semibold text-lg ">
+                      <span>FrontEnd</span>
+                    </Li>
+                    <Li className="flex justify-between pt-10">
+                      <span className="font-semibold">조성현</span>
+                      <GraySpan>99.10.14</GraySpan>
+                    </Li>
+
+                    <li className="h-[2px] bg-[#efefef] my-5"></li>
+                    <Li className="flex justify-between mb-[10px]">
+                      <span>정보처리산업기사</span>
+                      <GraySpan>2022.06</GraySpan>
+                    </Li>
+                    <Li>
+                      <span className="inline-block pb-1">
+                        계명문화대학교 컴퓨터학부
+                      </span>
+                      <GraySpan>2018.03~2020.08</GraySpan>
+                    </Li>
+                    <Li>
+                      <span className="inline-block pb-1">
+                        멀티캠퍼스 웹 개발자 수료
+                      </span>
+                      <GraySpan>2023.08~2024.02</GraySpan>
+                    </Li>
+
+                    <li className="flex justify-start mt-[25px] ml-[5px] ">
+                      {/* <span>Email</span> */}
+                      {/* <span className="text-gray-300 text-sm">
+                          sunghyun543@gmail.com
+                        </span> */}
+                      <div className="flex flex-col items-center">
+                        <span className="tracking-wide text-lg">KakaoQR</span>
+                        <img
+                          src={MyQR}
+                          alt="QRcode"
+                          className="max-w-[80px] max-h-[80px]"
+                        />
+                      </div>
+                    </li>
+                  </ul>
+                </FlipCardBack>
+              </FlipBox>
             </div>
           </div>
           <div className="overflow-hidden h-auto my-10" id="title">
             <h1
-              className="text-5xl text-white py-1
+              className="lg:text-5xl md:text-4xl mysm:text-3xl text-white py-1
               transform translate-y-[110px] animate-introSlideIn-title
               font-mono [text-shadow:_5px_4px_4px_#335154]"
             >
@@ -88,19 +139,39 @@ export const IntroduceTwo = ({ value }: IIntroduce) => {
     </div>
   );
 };
-
+const Li = styled.li`
+  padding: 0.7rem 0px;
+`;
+const GraySpan = styled.span`
+  color: rgb(209 213 219);
+`;
 const Img = styled.img`
   object-fit: cover;
   width: 100%;
-  height: 500px;
+  height: 100%;
+`;
+const Edge = styled.div`
+  border-top-left-radius: 45%;
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  right: 3.5rem;
+  bottom: -1.75rem;
+  z-index: 5;
+  transform: rotate(45deg);
+  @media screen and (min-width: 1024px) {
+    background-color: rgba(21, 136, 186, 0.98);
+  }
+  @media screen and (min-width: 375px) and (max-width: 1023px) {
+    background-color: rgba(18, 139, 185, 0.99);
+  }
 `;
 const FlipBox = styled.div<{ value: boolean }>`
   transition: 0.8s;
   transform-style: preserve-3d;
   position: relative;
-  min-width: 300px;
-  max-width: 300px;
-  height: 500px;
+  width: 100%;
+  height: 100%;
   transform: ${(props) => (props.value ? "rotateY(180deg)" : "rotateY(0deg)")};
 `;
 const FlipCardFront = styled.div`
@@ -110,7 +181,7 @@ const FlipCardFront = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  border: 1px solid gray;
+  /* border: 1px solid gray; */
 `;
 const FlipCardBack = styled.div`
   backface-visibility: hidden;
@@ -119,6 +190,6 @@ const FlipCardBack = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  border: 1px solid gray;
+
   transform: rotateY(180deg);
 `;
