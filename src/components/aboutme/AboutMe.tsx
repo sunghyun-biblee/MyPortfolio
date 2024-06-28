6e62392eb5050100b658cc06dc0824a1fd242b85
sunghyun1014 <sunghyun1014@naver.com>
2024-06-29 03:56:30 +0900

    수정: 텍스트 스크롤 애니메이션 추가

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -1,8 +1,9 @@
-import React, { forwardRef } from "react";
-import preview from "assets/img/profileImg.jpg";
+import React from "react";
+
 import preview2 from "assets/img/A1.jpg";
 import { ScrollMoveType } from "components/projects/Projects";
 import styled from "styled-components";
+import { useTextFadeIn } from "hooks/useTextFadeIn";
 
 interface IAboutMe {
   value: ScrollMoveType;
@@ -11,65 +12,69 @@ interface BackGroundProps {
   img: string;
 }
 export const AboutMe = ({ value }: IAboutMe) => {
-  // const login = () => {
-  //   window.location.href =
-  //     "https://api.devproject.store/oauth2/authorization/google";
-  // };
+  const TitleAnimate = useTextFadeIn(1, 0, "down");
+  const TextAnimate = useTextFadeIn(1, 0.4);
 
   return (
     <div
       className="h-[100vh] w-[100%] flex justify-center items-center
         bg-gradient-to-b from-cyan-500 to-[#20DABF] 
-      pt-[82px]
+      pt-[82px] 
       "
       ref={value.element}
     >
       <div
-        className="lg:w-[100%] mysm:w-[100vw] lg:max-w-[1200px]  lg:min-h-[90vh] mysm:min-h-[100vh] mx-auto my-0 relative flex flex-col justify-center text-left   box-border lg:p-3 mysm:p-0
-     lg:items-stretch mysm:items-center 
+        className="lg:w-[100%] mysm:w-[100vw] lg:max-w-[1400px]  lg:min-h-[100vh] mysm:min-h-[100vh] mx-auto my-0 relative flex flex-col justify-center text-left   box-border  mysm:p-0
+     lg:items-stretch mysm:items-center bg-transparent/10
       "
       >
         <BackGroundImg
           img={preview2}
-          className="lg:w-[500px] mysm:brightness-50   transition-all
-          duration-1000 
-           lg:brightness-90"
-          // onClick={login}
+          className="lg:w-[600px] transition-all duration-1000 lg:brightness-90  mysm:brightness-50"
         />
 
         <div
           className="z-10  lg:ml-[10%]  mysm:ml-[0px] 
-         transition-all duration-1000"
+         transition-all duration-1000 "
+          {...TitleAnimate}
         >
           <h1 className=" inline text-5xl text-white uppercase font-extrabold ">
             like a turtle
           </h1>
         </div>
         <div
-          className="flex flex-col md:text-base mysm:text-sm z-10  text-white leading-8 tracking-wide font-medium max-w-[650px] lg:mt-[10rem] mysm:mt-[6rem]
-         lg:ml-[auto]   mysm:ml-[0px] 
+          className="flex flex-col md:text-lg mysm:text-sm z-10  text-white  font-medium max-w-[730px] lg:mt-[10rem] mysm:mt-[6rem]
+         lg:ml-[auto]   mysm:ml-[0px]  mysm:mr-[0px] lg:mr-[3rem] 
           transition-all duration-[1000ms]
           transfrom lg:-translate-y-[100px] mysm:-translate-y-[0px]
-           lg:px-0 mysm:px-3
+           lg:px-3 lg:py-3 mysm:px-3 
+             
         "
+          {...TextAnimate}
         >
           {/* <span className="cursor-pointer ">renderSkill Animation</span> */}
           <div>
-            꾸준히 우상향할 프론트엔드 개발자 조성현 입니다
-            <br />
-            <p className="py-2">
+            <h3 className="md:text-xl mysm:text-[17px] font-semibold leading-10">
+              꾸준히 우상향할 프론트엔드 개발자 조성현 입니다
+            </h3>
+
+            <div className="py-2 md:block mysm:flex flex-col my-2">
               <span className="lg:text-[black] mysm:text-[#ee4949]  font-extrabold text-xl tracking-wide transition-colors duration-700">
                 "중요한건 꺽이지 않는 마음"
               </span>
-              이 문장은 항상 마음에 두고있는 문장입니다
+              <span className="text-sm pl-3 pt-2">
+                이 문장은 항상 마음에 두고있는 문장입니다
+              </span>
+            </div>
+            <p className="leading-8 tracking-normal">
+              개발자를 준비하면서 처음마주하는 기술들은 시간이 다소 걸리더라도
+              외울려하지않고, 최대한 이해하여 사용하려는 편이며
+              <br />
+              프로젝트를 진행하며 생기는 트러블<b>(이슈)</b>을 마주쳐도
+              회피하지않고, 팀원들과의 소통으로 일정 및 리소스등을 고려하여 어떤
+              것이 우선순위인지 유연하게 판단하며, 팀원들의 공감을 바탕으로
+              프로젝트를 끝까지 마무리 합니다.
             </p>
-            개발자를 준비하면서 처음마주하는 기술들은 시간이 다소 걸리더라도
-            외울려하지않고, 최대한 이해하여 사용하려는 편이며,
-            <br />
-            프로젝트를 진행하며 생기는 트러블<b>(이슈)</b>을 마주쳐도
-            회피하지않고, 팀원들과의 소통으로 일정 및 리소스등을 고려하여 어떤
-            것이 우선순위인지 유연하게 판단하며, 팀원들의 공감을 바탕으로
-            프로젝트를 끝까지 마무리 합니다.
           </div>
           <span className="py-4">
             인터랙티브한 웹과 제이콥의 법칙을 활용하여 UI/UX 디자인을
@@ -123,8 +128,8 @@ const BackGroundImg = styled.div<BackGroundProps>`
  
    */
   @media screen and (min-width: 1024px) {
-    background-size: 1000px auto;
-    background-position: 27% 90%;
+    background-size: 1100px auto;
+    background-position: 23% 90%;
   }
   @media screen and (min-width: 375px) and (max-width: 1023px) {
     width: 100%;
