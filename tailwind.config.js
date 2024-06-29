a154f7c9913110afefb627d801b3e1bfc991d298
sunghyun1014 <sunghyun1014@naver.com>
2024-06-29 23:54:08 +0900

    추가: 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ["Pretendard"],
+        Nanum: ["NanumSquare"],
+      },
       screens: {
         mysm: "375px",
       },
