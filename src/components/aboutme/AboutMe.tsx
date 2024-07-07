bcc06dfec90994f18c52d68c92118042c81b8a9a
sunghyun1014 <sunghyun1014@naver.com>
2024-07-08 00:52:44 +0900

    수정: AboutMe 텍스트 수정

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -64,56 +64,71 @@ export const AboutMe = ({ value }: IAboutMe) => {
         >
           {/* <span className="cursor-pointer ">renderSkill Animation</span> */}
           <div>
-            <h1 className="md:text-[1.7rem] mysm:text-xl font-semibold ">
-              꾸준히 우상향할 프론트엔드 개발자 조성현 입니다
+            <h1 className="md:text-3xl mysm:text-2xl font-semibold mb-5 ">
+              꾸준히 우상향할{" "}
+              <span
+                className="transition-all duration-500 lg:text-gray-800 mysm:text-gray-200 font-bold
+              lg:border-b-0 mysm:border-b-2
+              "
+              >
+                프론트엔드 개발자 조성현
+              </span>{" "}
+              입니다
             </h1>
 
-            <div className=" md:block mysm:flex flex-col mt-[1rem] md:mb-[0.5rem] mysm:mb-[1rem]">
+            {/* <div className=" md:block mysm:flex flex-col mt-[1rem] md:mb-[0.5rem] mysm:mb-[1rem]">
               <span className="lg:text-[black] mysm:text-[#ee4949]  font-extrabold text-xl tracking-wide transition-colors duration-700">
                 ' 중요한건 꺽이지 않는 마음 '
               </span>
               <span className="text-sm md:pl-3 mysm:pl-1 pt-2 tracking-wider lg:text-inherit mysm:text-gray-300">
                 이라는 신조를 항상 마음에 새기고 있습니다
               </span>
+            </div> */}
+            <div className="transition-all lg:text-lg mysm:text-base leading-[1.9rem] break-keep">
+              <p>
+                개발자로 성장하는 과정에서 React 와 같은 프론트엔드 프레임워크를
+                학습할 때, 단순한 암기보다는 컴포넌트 라이프사이클, 상태 관리와
+                같은 핵심 개념을 이해하고 응용하는 것을 중요시합니다
+              </p>
+              <p className="py-2">
+                프로젝트 진행 중 발생하는 버그 및 이슈를 식별하고, 디버깅 과정을
+                통해 백엔드 이슈로 확인된 경우, 원인 분석과 결과 보고를 명확히
+                전달한 후, 팀원들과 적극적으로 소통하여 문제해결에 도움이 될 수
+                있도록 기여합니다
+              </p>
+              <p>
+                이런 경험을 바탕으로 배움에 대한 열정을 잃지 않고 꾸준한 노력을
+                기반으로 <br />
+                멀티캠퍼스 수료과정에서
+                <BestSpan
+                  className="text-yellow-300 hover:cursor-pointer"
+                  onClick={() => setIsModal(true)}
+                >
+                  개인 최우수상
+                </BestSpan>
+                을 수상했습니다
+              </p>
             </div>
-            <p className="leading-[1.9rem] py-1">
-              개발자로 성장하는 과정에서 처음 마주하는 기술들에 시간이
-              걸리더라도 외우려 하지 않고 최대한 이해하여 사용하는 것을 선호
-              합니다.
-              <br />
-              프로젝트 진행 중 발생하는 문제에 대해서는 회피하지 않고 팀원들과
-              소통하여 해결하며, 일정과 리소스를 고려하여 우선순위를 유연하게
-              조정한 뒤, 팀원들의 공감을 바탕으로 프로젝트를 끝까지 마무리합니다
-            </p>
-            <p>
-              이런 경험을 바탕으로 배움에 대한 열정을 잃지 않고 꾸준한 노력을
-              기반으로 <br />
-              멀티캠퍼스 수료과정에서{" "}
-              <BestSpan
-                className="text-yellow-300 hover:cursor-pointer"
-                onClick={() => setIsModal(true)}
-              >
-                개인 최우수상
-              </BestSpan>
-              을 수상했습니다
-            </p>
           </div>
-          <div className="pt-[2rem] text-base relative">
+          <div className="pt-[2rem] md:pb-0 mysm:pb-[0.1rem] text-base relative">
             P.S 인터랙티브한 웹과
             <JaKobSpan onClick={() => setIsDescript((prev) => !prev)}>
               제이콥의 법칙
             </JaKobSpan>
             을 활용하여 UI/UX 디자인을 구현하는걸 좋아합니다
             <div
-              className={`absolute  -left-[0%] transition-width duration-1000 overflow-hidden  text-[#BEFBFF] break
+              className={`  -left-[0%] transition-width duration-1000 overflow-hidden  text-[#BEFBFF]
                 md:-bottom-[50%]
                 mysm:-bottom-[65%]
                 max-w-[600px] 
-                md:min-h-[24px] 
-                md:max-h-[24px]
+               
                 mysm:min-h-[24px] 
-                mysm:max-h-[48px]
-              ${isDescript ? "w-[100%] " : "w-[0px] "}`}
+                
+              ${
+                isDescript
+                  ? "w-[100%] mysm:max-h-[48px] "
+                  : "w-[0%] max-h-[1px]  md:max-h-[24px]"
+              }`}
             >
               사용자는 익숙해진 기존의 경험을 바탕으로 기대치를 형성하는 경향을
               보인다는 내용의 법칙
