2da413e17d4b9667ebcdb9b472c4ff6c817754ff
sunghyun1014 <sunghyun1014@naver.com>
2024-07-11 02:50:58 +0900

    추가: 프로젝트 상세 데이터 추가

---
+++ src/data/ProjectDetailDatas.ts
@@ -0,0 +1,9 @@
+import { ClassHubData } from "./DetailDatas/ClassHub";
+import { MyPortfolioData } from "./DetailDatas/MyPortFolio";
+import { TripFixiedData } from "./DetailDatas/TripFixied";
+
+export const ProjectDetailDatas = [
+  { ...ClassHubData },
+  { ...MyPortfolioData },
+  { ...TripFixiedData },
+];
